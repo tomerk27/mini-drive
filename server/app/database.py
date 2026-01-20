0b1e189bdc2575e0f324742bbbbaaf69c8f1f25d
Tomer Kaplan <tomer.kaplan2007@gmail.com>
2026-01-20 22:08:21 +0200

    SERVER - Set connection with the database

--- server/app/database.py
+++ server/app/database.py
@@ -1,3 +1,18 @@
 from dotenv import load_dotenv
-import motor.motor_asyncho
+import motor.motor_asyncio
 import os
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+DB_NAME = os.getenv("DB_NAME")
+
+if not MONGO_URL: 
+    raise ValueError("No MONGO_URL found in .env file")
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+
+db = client[DB_NAME]
+
+def get_user_collection():
+    return db["users"]
