9e213994d13d12e00eebcb6d99a26e2560773a54
Tomer Kaplan <tomer.kaplan2007@gmail.com>
2026-01-20 21:58:29 +0200

    SERVER - Use schema is added

---
+++ server/app/schemas/user.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class UserBase(BaseModel): 
+    username: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    id: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True # Allows Pydantic to read data from object attributes
